7c06dce07399f705bf4324a4b25086adca3039ef
Stefaan <safwebdev@gmail.com>
2020-10-08 18:30:58 +0800

    add controls

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -14,9 +14,12 @@ export const ResultCard = ({ movie }) => {
       <div className="info">
         <div className="header">
           <h3 className="title">{movie.title}</h3>
-          <h3 className="release-date">
+          <h4 className="release-date">
             {movie.release_date ? movie.release_date.substring(0, 4) : "NA"}
-          </h3>
+          </h4>
+        </div>
+        <div className="controls">
+          <button className="btn">Add to Watch List</button>
         </div>
       </div>
     </div>
